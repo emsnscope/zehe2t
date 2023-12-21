ec25764acaccde98d46b023bbe7b97585ecfcef5
emsnscope <154502831+emsnscope@users.noreply.github.com>
2023-12-21 10:25:25 +0200

    Add files via upload

---
+++ styles.css.css
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Pharmacies Accordion</title>
+</head>
+<body>
+
+<div class="accordion">
+    <!-- Pharmacy 1 -->
+    <div class="accordion-item">
+        <div class="accordion-header">Pharmacy Name 1</div>
+        <div class="accordion-content">
+            <img src="pharmacy_logo_1.png" alt="Pharmacy Logo 1">
+            <p>Location: [Include the address or link to Google Maps]</p>
+        </div>
+    </div>
+
+    <!-- Pharmacy 2 -->
+    <div class="accordion-item">
+        <div class="accordion-header">Pharmacy Name 2</div>
+        <div class="accordion-content">
+            <img src="pharmacy_logo_2.png" alt="Pharmacy Logo 2">
+            <p>Location: [Include the address or link to Google Maps]</p>
+        </div>
+    </div>
+
+    <!-- Pharmacy 3 -->
+    <div class="accordion-item">
+        <div class="accordion-header">Pharmacy Name 3</div>
+        <div class="accordion-content">
+            <img src="pharmacy_logo_3.png" alt="Pharmacy Logo 3">
+            <p>Location: [Include the address or link to Google Maps]</p>
+        </div>
+    </div>
+</div>
+
+<script src="script.js"></script>
+</body>
+</html>
